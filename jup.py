575bd62d9371f437237c8499c7dd63d94b90be88
Dinesh <dinesh@micropyramid.com>
2017-05-16 00:37:59 +0530

    Update jup.py

--- jup.py
+++ jup.py
@@ -1,1 +1,16 @@
-print('jupyterhub '*3)
+from sklearn import datasets
+from matplotlib import rcParams
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+#Set graph fonts
+rcParams['font.family'] = 'DejaVu Sans'
+
+#load boston dataset
+boston_dataset = datasets.load_boston()
+boston_data = pd.DataFrame(boston_dataset.data, columns=boston_dataset.feature_names)
+
+boston_sample = boston_data.head(10)
+boston_sample
